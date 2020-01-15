f29167b8c5e20c9a2ab1d8ab87d9fe8e2b1ac124
Xett <3954937+Xett@users.noreply.github.com>
2020-01-15 14:56:14 +1000

    Add project template files

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
 import setuptools
-
-with open("README.md", "r") as fh:
-    long_description=fh.read()
-
+with open("README.md","r") as fh:
+	long_description=fh.read()
 setuptools.setup(
-    name='hexmap',
-    version='0.1',
-    scripts=['bin/hexmap'],
-    author='Xet',
-    author_email='ethan.jones@my.jcu.edu.au',
-    description='Hexmap storage and coordinate system',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Xett/Hexmap",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License"
-        "Operating System :: OS Independent",
-    ]
+	name='hexmap',
+	version='0.1',
+	scripts=['bin/hexmap'],
+	author='Xett',
+	author_email='3954937+Xett@users.noreply.github.com',
+	description='Hexmap storage and coordinates',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url='https://github.com/Xett/hexmap.git',
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	]
 )

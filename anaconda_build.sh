8040101121ded8bf388660be73132bb80c74b64d
Xett <3954937+Xett@users.noreply.github.com>
2020-02-01 23:53:36 +1000

    Fix build script

--- anaconda_build.sh
+++ anaconda_build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 conda build .
-conda install --use-local -y --force-reinstall $(git config --local remote.origin.url|sed -n 's#.*/\([^.]*\)\.git#\1#p')
+conda install --use-local -y --force-reinstall $(git config --local remote.origin.url|sed -n 's#.*/\([^.]*\)\#\1#p')
 conda build purge
